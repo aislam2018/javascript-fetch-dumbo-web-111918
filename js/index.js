c2335296cc08275206be35990aa24a00d65808b7
Anik Islam <Anik1606@gmail.com>
2019-01-11 11:51:31 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${app}`
+  }
+}).then(res => res.json()).then(json => console.log(json))
